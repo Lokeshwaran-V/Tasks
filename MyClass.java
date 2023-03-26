80a1537988281e880327a669df6617c6dde3f5fe
Lokeshwaran-V <127390823+Lokeshwaran-V@users.noreply.github.com>
2023-03-26 20:51:37 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,43 @@
+package Lokesh;
+
+import java.sql.*;
+
+
+public class MyClass {
+	public static void main (String [] arg) {
+		try {
+//			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Students","root","root");
+			Statement stmt = con.createStatement();
+			
+			String in = "insert into register values(005,'lll',555)"; 
+			stmt.executeUpdate(in);
+			
+			in = "insert into register values(006,'ooo',666)"; 
+			stmt.executeUpdate(in);
+			
+			in = "insert into register values(007,'ppp',777)"; 
+			stmt.executeUpdate(in);
+			
+			ResultSet rs = stmt.executeQuery("select * from register");
+			
+			while (rs.next()) {
+				System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+			}
+			
+			String dl = "delete from register " + "WHERE S_Name = 'lll'"; 
+			stmt.executeUpdate(dl);
+			
+			rs = stmt.executeQuery("select * from register");
+			
+			System.out.println("---------After deletion----------");
+			while (rs.next()) {
+				System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+			}
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	}
+
+}
